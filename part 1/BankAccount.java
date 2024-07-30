5c88a68cd4221d43775e80f64b23b75393efef1f
GaralaDhruvi <23dcs029@charusat.edu.in>
2024-07-31 00:38:41 +0530

    Create BankAccount.java

---
+++ part 1/BankAccount.java
@@ -0,0 +1,10 @@
+public class BankAccount
+ {
+    public static void main(String[] args) {
+    
+        double currentBalance = 20.0;
+
+    
+        System.out.println("The current balance is $" + currentBalance);
+    }
+}
